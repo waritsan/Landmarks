09c99a3fa08acef7ff42c9da468623d53fefb1ae
Warit Santaputra <waritsan@gmail.com>
2021-02-07 10:31:59 +0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Warit Santaputra on 7/2/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
